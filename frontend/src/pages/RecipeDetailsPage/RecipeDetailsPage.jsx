2957511a1505d25baba6e3c658a57ed71f3fb4d4
Richard Mundy <mundy.richard@icloud.com>
2024-07-17 21:09:10 -0700

    add RecipeDetailsPage

--- frontend/src/pages/RecipeDetailsPage/RecipeDetailsPage.jsx
+++ frontend/src/pages/RecipeDetailsPage/RecipeDetailsPage.jsx
@@ -1,7 +1,19 @@
-import { Container, Box, Paper } from "@mui/material";
+import {
+  Container,
+  Box,
+  Paper,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Typography,
+} from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { getRecipeById } from "../../features/recipes/recipeSlice";
 import { useParams } from "react-router-dom";
+import NoImage from "../../assets/images/no_image.jpg";
 import React, { useEffect } from "react";
 
 export default function RecipeDetailsPage() {
@@ -17,13 +29,56 @@ export default function RecipeDetailsPage() {
 
   console.log(recipe);
 
+  function formattedDated(date) {
+    const day = date.slice(8, 10);
+    const month = date.slice(5, 7);
+    const year = date.slice(2, 4);
+
+    return month + "/" + day + "/" + year;
+  }
+
   return (
     <>
       <Container>
         <Box>
           <Paper>
-            <h1>Recipe Details</h1>
+            <h1>{recipe.name} Details</h1>
+            {recipe.image ? (
+              <img src={recipe.image} alt="food...yummy" />
+            ) : (
+              <img src={NoImage} alt="No Image Available" />
+            )}
+            <h4>Recipe Submitted by: {recipe.submitted_by}</h4>
+            <Box>
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell align="center">Ingredients</TableCell>
+                      <TableCell align="center">Quantity</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {recipe.ingredients.map((item, idx) => (
+                      <TableRow key={idx}>
+                        <TableCell align="center">{item.name}</TableCell>
+                        <TableCell align="center">{item.quantity}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Box>
           </Paper>
+          <Box>
+            <Paper sx={{ p: 5 }}>
+              <Typography variant="h6">Steps</Typography>
+              <Typography variant="h8">{recipe.steps}</Typography>
+            </Paper>
+          </Box>
+          <Typography>
+            Submitted on: {formattedDated(recipe.created_at)}
+          </Typography>
         </Box>
       </Container>
     </>
